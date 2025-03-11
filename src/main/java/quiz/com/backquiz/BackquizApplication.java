d3e7456f77102f594474ae972e6c1355eb7e11b6
matheus <pachecx35@gmail.com>
2025-03-11 15:23:16 -0300

    first commit

---
+++ src/main/java/quiz/com/backquiz/BackquizApplication.java
@@ -0,0 +1,13 @@
+package quiz.com.backquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/quiz/com/backquiz/BackquizApplicationTests.java
@@ -0,0 +1,13 @@
+package quiz.com.backquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
